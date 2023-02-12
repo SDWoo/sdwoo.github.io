ab83fff39f019dd139582dd60840bd910f4e8784
sdwoo <damjang67@naver.com>
2023-02-12 16:17:02 +0900

    feat: utterence -> giscus로 댓글 폼 변경

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useRef } from 'react';
 import styled from '@emotion/styled';
 
-const src = 'https://utteranc.es/client.js';
+const src = 'https://giscus.app/client.js';
 const repo = 'SDWoo/sdwoo.github.io';
 
 type UtteranceAttributesType = {
   src: string;
-  repo: string;
-  'issue-term': string;
-  label: string;
-  theme: string;
+  'data-repo': string;
+  'data-repo-id': string;
+  'data-category': string;
+  'data-category-id': string;
+  'data-mapping': string;
+  'data-strict': string;
+  'data-reactions-enabled': string;
+  'data-emit-metadata': string;
+  'data-input-position': string;
+  'data-theme': string;
+  'data-lang': string;
   crossorigin: string;
   async: string;
 };
 
-const UtterancesWrapper = styled.div`
+const GiscusWrapper = styled.div`
   @media (max-width: 768px) {
     padding: 0 20px;
   }
@@ -25,27 +32,34 @@ const CommentWidget = () => {
 
   useEffect(() => {
     if (element.current === null) return;
-    const utterances: HTMLScriptElement = document.createElement('script');
+    const giscus: HTMLScriptElement = document.createElement('script');
 
     const attributes: UtteranceAttributesType = {
       src,
-      repo,
-      'issue-term': 'pathname',
-      label: 'comments',
-      theme: 'github-light',
+      'data-repo': repo,
+      'data-repo-id': 'R_kgDOI3JM_Q',
+      'data-category': 'Comments',
+      'data-category-id': 'DIC_kwDOI3JM_c4CUJ2Y',
+      'data-mapping': 'title',
+      'data-strict': '0',
+      'data-reactions-enabled': '1',
+      'data-emit-metadata': '0',
+      'data-input-position': 'bottom',
+      'data-theme': 'preferred_color_scheme',
+      'data-lang': 'ko',
       crossorigin: 'anonymous',
       async: 'true',
     };
 
     // html 이 없어서 이렇게 따로 넣어줘야되나 봄
     Object.entries(attributes).forEach(([key, value]) => {
-      utterances.setAttribute(key, value);
+      giscus.setAttribute(key, value);
     });
 
-    element.current.appendChild(utterances);
+    element.current.appendChild(giscus);
   }, []);
 
-  return <UtterancesWrapper ref={element} />;
+  return <GiscusWrapper ref={element} />;
 };
 
 export default CommentWidget;
